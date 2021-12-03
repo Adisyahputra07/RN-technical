f16122ec13b3f441aaccf4d2b6b8d8b193aa36c4
adilubis <syahputra631@gmail.com>
2021-12-04 06:11:39 +0700

    create feature search

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { NativeBaseProvider } from "native-base";
-import CallingApi from "./src/Main";
+import CallingApi from "./src/Index";
 import Nav from "./src/Nav";
 
 export default function App() {
@@ -9,5 +9,5 @@ export default function App() {
       <Nav />
       <CallingApi />
     </NativeBaseProvider>
-  
+  );
 }
